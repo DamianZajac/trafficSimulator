0de13e926a2ea82a7f07eec20d21072a26903314
Kacper Stąpor <endzior93@gmail.com>
2015-05-31 16:10:01 +0200

    Road gfx update
    Forgot one way connected roads, added horizontal and vertical divided
    delays to traffic lights

--- objects/traffic_light.py
+++ objects/traffic_light.py
@@ -2,14 +2,15 @@ class Traffic_Light:
 	GREEN = True
 	RED = False
 	
-	def __init__(self, street_parent, switch_delay = 15):
+	def __init__(self, street_parent, switch_delay_horizontal = 15, switch_delay_vertical = 15):
 		self.street_parent = street_parent
 		self.horizontal_light = Traffic_Light.GREEN
 		self.vertical_light = Traffic_Light.RED
-		self.switch_delay = switch_delay
+		self.switch_delay_horizontal = switch_delay_horizontal
+		self.switch_delay_vertical = switch_delay_vertical
 		
 	def print_header(self):
-		print("Street Parent\t|\tHorizontal Status\t|\tVertical Status")
+		print("Street Parent\t|\tHorizontal Status and delay\t|\tVertical Status and delay")
 		return self
 		
 	def print_singular(self):
@@ -18,7 +19,7 @@ class Traffic_Light:
 		return self
 		
 	def print_multi(self):
-		print("%s\t|\t%s\t|\t%s" % (self.street_parent, self.horizontal_light, self.vertical_light))
+		print("%s\t|\t%s %s\t|\t%s %s" % (self.street_parent, self.horizontal_light, self.switch_delay_horizontal, self.vertical_light, self.switch_delay_vertical))
 		return self
 		
 	def switch_lights(self):
@@ -26,9 +27,16 @@ class Traffic_Light:
 		self.vertical_light = not self.vertical_light
 		return self
 		
-	def get_delay(self):
-		return self.switch_delay
+	def get_delay_horizontal(self):
+		return self.switch_delay_horizontal
+	
+	def get_delay_horizontal(self):
+		return self.switch_delay_vertical
 		
-	def set_delay(self, switch_delay):
-		self.switch_delay = switch_delay
+	def set_delay_vertical(self, switch_delay):
+		self.switch_delay_vertical = switch_delay
+		return self
+		
+	def set_delay_vertical(self, switch_delay):
+		self.switch_delay_vertical = switch_delay
 		return self
